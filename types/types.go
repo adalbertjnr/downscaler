f97d9eca4ffbf9b1d77a7e80764822c2ecc7d53b
Adalberto Junior <adalberto.gmu@gmail.com>
2024-06-06 12:31:51 -0300

    add new type

--- types/types.go
+++ types/types.go
@@ -32,10 +32,9 @@ type CmManifest struct {
 	} `yaml:"data"`
 }
 
-type DeschedulerPolicy struct {
-	APIVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
-	Spec       struct {
+type DownscalerPolicy struct {
+	Kind string `yaml:"kind"`
+	Spec struct {
 		ExecutionOpts struct {
 			Time struct {
 				DefaultUptime string `yaml:"defaultUptime"`
@@ -44,12 +43,20 @@ type DeschedulerPolicy struct {
 			} `yaml:"time"`
 			Downscaler struct {
 				DownscalerSelectorTerms struct {
-					MatchExpressions []struct {
+					MatchExpressions struct {
 						Key      string   `yaml:"key"`
 						Operator string   `yaml:"operator"`
 						Values   []string `yaml:"values"`
 					} `yaml:"matchExpressions"`
 				} `yaml:"downscalerSelectorTerms"`
+				WithAdvancedNamespaceOpts struct {
+					MatchCriteria struct {
+						Criteria []struct {
+							Namespaces []string `yaml:"namespaces"`
+							WithCron   string   `yaml:"withCron"`
+						} `yaml:"criteria"`
+					} `yaml:"matchCriteria"`
+				} `yaml:"withAdvancedNamespaceOpts"`
 			} `yaml:"downscaler"`
 		} `yaml:"executionOpts"`
 	} `yaml:"spec"`
