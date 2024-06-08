c723aa49c9f5b97885eb61f8ed6a30751ff7ffa0
Adalberto Junior <adalberto.gmu@gmail.com>
2024-06-08 10:16:47 -0300

    new flagname for cmname

--- input/flags.go
+++ input/flags.go
@@ -1,13 +1,21 @@
 package input
 
-import "flag"
+import (
+	"flag"
+	"log/slog"
+	"os"
+)
 
 type FromFlags struct {
 	InitialCmConfig string
 }
 
 func Flags() *FromFlags {
-	InitialCmPath := flag.String("initial_config_path", "config/configmap.yaml", "set the initial configmap config path")
+	InitialCmPath := flag.String("configmap_name", "", "set the configmap name")
 	flag.Parse()
+	if *InitialCmPath == "" {
+		slog.Error("validate deployment args", "error", "missing configmap name")
+		os.Exit(1)
+	}
 	return &FromFlags{InitialCmConfig: *InitialCmPath}
 }
