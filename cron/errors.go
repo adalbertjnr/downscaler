8daa4f6c9d6f5e3d7abb6b200c31b73dfa9f34ce
Adalberto Junior <adalberto.gmu@gmail.com>
2024-06-08 14:46:53 -0300

    const errors

---
+++ cron/errors.go
@@ -0,0 +1,10 @@
+package cron
+
+const (
+	ErrDefaultUpTimeNotFound      = "default uptime is missing"
+	ErrTimeZoneNotFound           = "time zone is missing"
+	ErrRecurrenceTimeNotFound     = "recurrence time is missing"
+	ErrNotValidExpressionKey      = "not valid expression key"
+	ErrNotValidExpressionOperator = "not valid expression operator"
+	ErrExpressionsValuesAreEmpty  = "the expression values are empty"
+)
