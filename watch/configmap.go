1b28ab19b68ba2e478de3a8abd2c03b7f6ab2ec8
Adalberto Junior <adalberto.gmu@gmail.com>
2024-06-08 14:46:38 -0300

    logs configmap watcher

--- watch/configmap.go
+++ watch/configmap.go
@@ -3,6 +3,7 @@ package watch
 import (
 	"log/slog"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 )
@@ -11,10 +12,16 @@ func ConfigMap(watcher watch.Interface, cmObjectch chan runtime.Object) {
 	for event := range watcher.ResultChan() {
 		switch event.Type {
 		case watch.Modified:
-			slog.Info("received new modified object", "resourceType", "configMap")
+			cm := event.Object.(*corev1.ConfigMap)
+			slog.Info("configmap was updated",
+				"name", cm.Name,
+				"namespace", cm.Namespace,
+			)
 			cmObjectch <- event.Object
 		case watch.Error:
-			slog.Error("error updating the object", "resourceType", "configMap")
+			slog.Error("error updating the object",
+				"resource type", "configmap",
+			)
 		}
 	}
 }
