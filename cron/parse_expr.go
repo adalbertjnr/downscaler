5d12ba086a0fcc43c62095b1ee1ce64754dbf56f
Adalberto Junior <adalberto.gmu@gmail.com>
2024-06-12 17:56:58 -0300

    validation to show ignored namespaces

--- cron/parse_expr.go
+++ cron/parse_expr.go
@@ -1,5 +1,7 @@
 package cron
 
+import "log/slog"
+
 type DownscalerExpression struct {
 	MatchExpressions struct {
 		Key      string   `yaml:"key"`
@@ -10,6 +12,28 @@ type DownscalerExpression struct {
 
 func (v *DownscalerExpression) withExclude() bool {
 	return v.MatchExpressions.Operator == "exclude"
+}
+
+func (v *DownscalerExpression) showIgnoredNamespaces(ignoredNamespaces []string) map[string]struct{} {
+	if len(ignoredNamespaces) > 0 {
+		excludeNamespaceMap := make(map[string]struct{})
+		for _, namespace := range ignoredNamespaces {
+			slog.Info("namespace validator",
+				"namespace", namespace,
+				"status", "ignored during crontime scheduling routine",
+			)
+			if _, exists := excludeNamespaceMap[namespace]; !exists {
+				excludeNamespaceMap[namespace] = struct{}{}
+			} else {
+				continue
+			}
+		}
+		return excludeNamespaceMap
+	}
+	slog.Info("namespace validator",
+		"message", "no namespace was provided to be ignored during scheduling",
+	)
+	return nil
 }
 
 type DownscalerCriteria struct {
